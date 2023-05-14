8fcdf5ac058979716cdc8878b49d6e8a144da8c8
maiOSX <124741903+maiOSX@users.noreply.github.com>
2023-05-14 13:06:52 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+       This is an example update.
+      
       </main>
 
       <Footer />
